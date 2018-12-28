11751ab47a2db0c999e07ed265ba5ef3c9852707
z1995 <zhannaavanesova@yahoo.com>
2018-12-27 20:11:42 -0700

    Changes

--- liri.js
+++ liri.js
@@ -5,9 +5,11 @@ var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
 var moment = require("moment");
 
+var input = process.argv[2];
+var info = process.argv.slice(3).join(" ");
+
 var music = function() {
-    var artist = process.argv.slice(3).join(" ");
-    var URL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    var URL = "https://rest.bandsintown.com/artists/" + info + "/events?app_id=codingbootcamp";
 
     axios.get(URL).then(function(response) {
         for(var i=0; i < 5; i++){
@@ -18,7 +20,28 @@ var music = function() {
         }
     })
 }
-var input = process.argv[2];
+
+var movie = function() {
+    var URL = "http://www.omdbapi.com/?t=" + info + "&y=&plot=short&apikey=trilogy";
+    axios.get(URL).then(function(response) {
+        console.log(response.data);
+    })
+}
+
 if(input === "concert-this"){
     music();
+}else if(input === "movie-this"){
+    movie();
+}
+
+if(input === "song-this"){
+    spotify.search({ type: 'track', query: info }, function(err, response) {
+        if ( err ) {
+            console.log('Error occurred: ' + err);
+            return;
+        } else{
+            console.log(response.tracks.items[0].album.artists[0].name);
+        }
+    });
+
 }
